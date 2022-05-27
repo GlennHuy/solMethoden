395a9ba9fa37f46262ec699d935bab893b928805
Glenn H <huygelberghs.g@hotmail.be>
2022-05-27 15:30:20 +0200

    Begonnen met assignment Muziek en Methoden Enkel nog interne variabelen aan de methoden toekennen

---
+++ assignmentMuziekEnMethoden/Note.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentMuziekEnMethoden
+{
+    internal class Note
+    {
+        public void Do()
+        {
+            Console.Beep(264, 1000);
+        }
+        public void Re()
+        {
+            Console.Beep(297, 1000);
+        }
+        public void Mi()
+        {
+            Console.Beep(330, 1000);
+        }
+        public void Fa()
+        {
+            Console.Beep(352, 1000);
+
+        }
+        public void Sol()
+        {
+            Console.Beep(396, 1000);
+
+        }
+        public void La()
+        {
+            Console.Beep(440, 1000);
+        }
+        public void Si()
+        {
+            Console.Beep(495, 1000);
+        }
+        public void Do2()
+        {
+            Console.Beep(528, 1000);
+        }
+    }
+}
---
+++ assignmentMuziekEnMethoden/Program.cs
@@ -0,0 +1,70 @@
+﻿using assignmentMuziekEnMethoden;
+
+void Do(int hertz, int duration)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Do");
+    Console.ResetColor();
+    Console.Beep(hertz, duration);
+}
+void Re()
+{
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("Re");
+    Console.ResetColor();
+    Console.Beep(297, 1000);
+}
+void Mi()
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Mi");
+    Console.ResetColor();
+    Console.Beep(330, 1000);
+}
+void Fa()
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.WriteLine("Fa");
+    Console.ResetColor();
+    Console.Beep(352, 1000);
+
+}
+void Sol()
+{ 
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Sol");
+    Console.ResetColor();
+
+    Console.Beep(396, 1000);
+
+}
+void La()
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("La");
+    Console.ResetColor();
+    Console.Beep(440, 1000);
+}
+void Si()
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("Si");
+    Console.ResetColor();
+    Console.Beep(495, 1000);
+}
+void Do2()
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine("Do");
+    Console.ResetColor();
+    Console.Beep(528, 1000);
+}
+
+Do(253, 500);
+Re();
+Mi();
+Fa();
+Sol();
+La();
+Si();
+Do2();
