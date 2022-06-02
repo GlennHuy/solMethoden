86ec105db71c1a48aa3b6b2b656fc6e057d0679b
Glenn H <huygelberghs.g@hotmail.be>
2022-06-02 11:06:07 +0200

    Assignment Muziek en Methoden

--- assignmentMuziekEnMethoden/Note.cs
+++ assignmentMuziekEnMethoden/Note.cs
@@ -8,28 +8,28 @@ namespace assignmentMuziekEnMethoden
 {
     internal class Note
     {
-        public void Do(int duration = 1000, int octave = 1)
+        public  void Do(int duration = 1000, int octave = 1)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Do");
             Console.ResetColor();
             Console.Beep((253 * octave), duration);
         }
-        static void Re(int duration = 1000, int octave = 1)
+        public  void Re(int duration = 1000, int octave = 1)
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Re");
             Console.ResetColor();
             Console.Beep(297 * octave, duration);
         }
-        void Mi(int duration = 1000, int octave = 1)
+        public  void Mi(int duration = 1000, int octave = 1)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Mi");
             Console.ResetColor();
             Console.Beep(330 * octave, duration);
         }
-        void Fa(int duration = 1000, int octave = 1)
+        public  void Fa(int duration = 1000, int octave = 1)
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Fa");
@@ -37,30 +37,29 @@ namespace assignmentMuziekEnMethoden
             Console.Beep(352 * octave, duration);
 
         }
-        void Sol(int duration = 1000, int octave = 1)
+        public  void Sol(int duration = 1000, int octave = 1)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Sol");
             Console.ResetColor();
-
             Console.Beep(396 * octave, duration);
 
         }
-        void La(int duration = 1000, int octave = 1)
+        public  void La(int duration = 1000, int octave = 1)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("La");
             Console.ResetColor();
             Console.Beep(440 * octave, duration);
         }
-        void Si(int duration = 1000, int octave = 1)
+        public  void Si(int duration = 1000, int octave = 1)
         {
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("Si");
             Console.ResetColor();
             Console.Beep(495 * octave, duration);
         }
-        void Do2(int duration = 1000, int octave = 1)
+        public  void Do2(int duration = 1000, int octave = 1)
         {
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
             Console.WriteLine("Do");
---
+++ assignmentMuziekEnMethoden/Playlist.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentMuziekEnMethoden
+{
+    public class Playlist
+    {
+        Note instance;
+
+        public Playlist()
+        {
+            instance = new Note();
+        }
+
+
+        private void Log(string input)
+        {
+            Console.WriteLine($"playing song: {input}");
+        }
+
+        public void SpeelYouAreMySunshine()
+        {
+            Log("You Are My Sunshine");
+            instance.Re();
+            instance.Sol();
+            instance.La();
+            instance.Si();
+            instance.Si();
+            instance.Si();
+            instance.La();
+            instance.Si();
+            instance.Sol();
+            instance.Sol();
+
+
+            instance.Sol();
+            instance.La();
+            instance.Si();
+            instance.Do();
+            instance.Mi();
+            instance.Mi();
+            instance.Re();
+            instance.Do();
+            instance.Si();
+        }
+    }
+}
